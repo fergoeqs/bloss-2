5f094c9827173dd9614fe6dab718e5b6f31a5eb0
eoeqs <sashaivanova977@gmail.com>
2025-03-15 17:15:01 +0300

    fixes

--- src/main/java/org/fergoeqs/blps1/services/VacancyService.java
+++ src/main/java/org/fergoeqs/blps1/services/VacancyService.java
@@ -2,7 +2,6 @@ package org.fergoeqs.blps1.services;
 
 import org.fergoeqs.blps1.models.Vacancy;
 import org.fergoeqs.blps1.repositories.VacancyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
