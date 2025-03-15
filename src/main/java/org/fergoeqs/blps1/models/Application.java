16d4f452cad19ee0867f9cc684fb114f3656617a
eoeqs <sashaivanova977@gmail.com>
2025-03-15 17:15:01 +0300

    fixes

--- src/main/java/org/fergoeqs/blps1/models/Application.java
+++ src/main/java/org/fergoeqs/blps1/models/Application.java
@@ -1,5 +1,6 @@
 package org.fergoeqs.blps1.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -13,14 +14,17 @@ public class Application {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vacancy_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Vacancy vacancy;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "applicant_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Applicant applicant;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "resume_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Resume resume;
 
     @Column(name = "cover_letter", columnDefinition = "TEXT")
